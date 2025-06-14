00331371854ec5dcd488df72fb6071eeaae58dcf
gilla5 <156371739+gilla5@users.noreply.github.com>
2025-06-13 22:54:05 -0400

    Create Story.js

---
+++ javascript/Story.js
@@ -0,0 +1,21 @@
+let adjective = "Large";
+let noun = "bear";
+let verb = "breathing";
+let place = "forest";
+let adjective2 = "shiny";
+let noun2 = "fish";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "strong";
+noun = "shark";
+verb = "hunting";
+place = "ocean";
+adjective2 = "long";
+noun2 = "shrimp";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
