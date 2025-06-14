464c09c4fd49caa8c9ecfc099053fa96246563f8
gilla5 <156371739+gilla5@users.noreply.github.com>
2025-06-13 22:32:36 -0400

    Create TriviaBot.js

---
+++ javascript/TriviaBot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Eren"
+let botLocation = "Clouds"
+let favoriteLanguage = "Java Script"
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "A fun fact about " + favoriteLanguage + " is that it was created in just 10 days by Brendan Eich at Netscape in 1995.";
+
+console.log(codingFact);
+
+codingFact = "Over 98% of websites use " + favoriteLanguage + " on the client side. That means if you're browsing the web, you're using it constantly.";
+
+console.log(codingFact);
+
+codingFact = "Despite the name, " + favoriteLanguage + " has nothing to do with Java. The name was a marketing move to ride Java's popularity at the time.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
